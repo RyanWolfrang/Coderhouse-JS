3c38f0560286b6f1bb56963297febda0945db895
Ryan <agustinrubertoni@gmail.com>
2022-10-11 18:22:39 -0300

    Incorporo filtros básicos

--- js/script.js
+++ js/script.js
@@ -61,26 +61,26 @@ class creadorMascotas{
 }
 const mascotas = [];
 
-mascotas.push(new creadorMascotas("Lena", "2 años", "Negro", "3.200"));
-mascotas.push(new creadorMascotas("Roger", "8 años", "Negro", "7.800"));
-mascotas.push(new creadorMascotas("Killi", "2 años", "Tricolor", "3.800"));
+mascotas.push(new creadorMascotas("Lena", 2, "Negro", "3.200"));
+mascotas.push(new creadorMascotas("Roger", 8, "Negro", "7.800"));
+mascotas.push(new creadorMascotas("Killi", 2, "Tricolor", "3.800"));
 
 console.log(mascotas[0].nombre)
 console.log(mascotas[1].nombre)
 console.log(mascotas[2].nombre)
-
+let buscarGato = prompt("Ingrese el nombre del gato (Ingrese FIN para cerrar)");
 do{
-    let buscarGato = prompt("Ingrese el nombre del gato (Ingrese FIN para cerrar)");
+    
 if((buscarGato == "LENA") || (buscarGato == "Lena" || (buscarGato == "lena"))){
-    alert("Elegiste a "+mascotas[0].nombre+" Una gata de color "+mascotas[0].color+" con "+mascotas[0].edad+" de edad.")
+    alert("Elegiste a "+mascotas[0].nombre+" Una gata de color "+mascotas[0].color+" con "+mascotas[0].edad+" años de edad.")
 } else if ((buscarGato == "ROGER") || (buscarGato == "Roger" || (buscarGato == "roger"))){
-    alert("Elegiste a "+mascotas[1].nombre+" Un gato de color "+mascotas[1].color+" con "+mascotas[1].edad+" de edad.")
+    alert("Elegiste a "+mascotas[1].nombre+" Un gato de color "+mascotas[1].color+" con "+mascotas[1].edad+" años de edad.")
 } else if ((buscarGato == "KILLI") || (buscarGato == "Killi" || (buscarGato == "killi"))){
-    alert("Elegiste a "+mascotas[2].nombre+" Una gata de color "+mascotas[2].color+" con "+mascotas[2].edad+" de edad.")
+    alert("Elegiste a "+mascotas[2].nombre+" Una gata de color "+mascotas[2].color+" con "+mascotas[2].edad+" años de edad.")
 } else if ((buscarGato == "TODOS") || (buscarGato == "Todos" || (buscarGato == "todos"))){
-    alert("¡Elegiste a todos! ahí van: \n"+mascotas[0].nombre+" Una gata de color "+mascotas[0].color+" con "+mascotas[0].edad+" de edad.\n"+
-    mascotas[1].nombre+" Una gata de color "+mascotas[1].color+" con "+mascotas[1].edad+" de edad.\n"+
-    mascotas[2].nombre+" Una gata de color "+mascotas[2].color+" con "+mascotas[2].edad+" de edad.")
+    alert("¡Elegiste a todos! ahí van: \n"+mascotas[0].nombre+" Una gata de color "+mascotas[0].color+" con "+mascotas[0].edad+" años de edad.\n"+
+    mascotas[1].nombre+" Una gata de color "+mascotas[1].color+" con "+mascotas[1].edad+" años de edad.\n"+
+    mascotas[2].nombre+" Una gata de color "+mascotas[2].color+" con "+mascotas[2].edad+" años de edad.")
 } else {
     alert("Input inválido. Intentá de nuevo")
 }}while (buscarGato != "FIN")
@@ -102,3 +102,17 @@ if((buscarGato == "LENA") || (buscarGato == "Lena" || (buscarGato == "lena"))){
 } else {
     alert("Input inválido. Intentá de nuevo")
 }*/
+//Meter un filtro con los colores de los gatos
+const filtrarPorColorN = mascotas.filter((mascota) => mascota.color == "Negro");
+console.log(filtrarPorColorN);
+const filtrarPorColorT = mascotas.filter((mascota) => mascota.color == "Tricolor");
+console.log(filtrarPorColorT);
+const filtrarPorColorG = mascotas.filter((mascota) => mascota.color == "Gris");
+console.log(filtrarPorColorG);
+const filtrarPorColorBN = mascotas.filter((mascota) => mascota.color == "Blanco y negro");
+console.log(filtrarPorColorBN);
+
+//Filtrar por más jovenes a más viejos
+
+mascotas.sort((a,b) => a.edad - b.edad);
+console.log(mascotas);
