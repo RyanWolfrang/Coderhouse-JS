12b2287ae8e1763fb22ab3d71d05fe78afa1db00
Ryan <agustinrubertoni@gmail.com>
2022-10-13 10:47:50 -0300

    Acomodo el código para empezar con DOM

--- js/script.js
+++ js/script.js
@@ -65,9 +65,9 @@ mascotas.push(new creadorMascotas("Lena", 2, "Negro", "3.200"));
 mascotas.push(new creadorMascotas("Roger", 8, "Negro", "7.800"));
 mascotas.push(new creadorMascotas("Killi", 2, "Tricolor", "3.800"));
 
-console.log(mascotas[0].nombre)
-console.log(mascotas[1].nombre)
-console.log(mascotas[2].nombre)
+console.log(mascotas[0])
+console.log(mascotas[1])
+console.log(mascotas[2])
 
 do{
     var buscarGato = prompt("Ingrese el nombre del gato (Ingrese FIN para cerrar)");
@@ -82,24 +82,6 @@ if((buscarGato == "LENA") || (buscarGato == "Lena" || (buscarGato == "lena"))){
     mascotas[1].nombre+" Una gata de color "+mascotas[1].color+" con "+mascotas[1].edad+" años de edad.\n"+
     mascotas[2].nombre+" Una gata de color "+mascotas[2].color+" con "+mascotas[2].edad+" años de edad.")
 }}while (buscarGato != "FIN")
-
-
-//Esta iteración es lo que se usaría con DOM para no tener un do while todo turbio.
-/*
-let buscarGato = prompt("Ingrese el nombre del gato (Ingrese FIN para cerrar)");
-if((buscarGato == "LENA") || (buscarGato == "Lena" || (buscarGato == "lena"))){
-    alert("Elegiste a "+mascotas[0].nombre+" Una gata de color "+mascotas[0].color+" con "+mascotas[0].edad+" de edad.")
-} else if ((buscarGato == "ROGER") || (buscarGato == "Roger" || (buscarGato == "roger"))){
-    alert("Elegiste a "+mascotas[1].nombre+" Un gato de color "+mascotas[1].color+" con "+mascotas[1].edad+" de edad.")
-} else if ((buscarGato == "KILLI") || (buscarGato == "Killi" || (buscarGato == "killi"))){
-    alert("Elegiste a "+mascotas[2].nombre+" Una gata de color "+mascotas[2].color+" con "+mascotas[2].edad+" de edad.")
-} else if ((buscarGato == "TODOS") || (buscarGato == "Todos" || (buscarGato == "todos"))){
-    alert("¡Elegiste a todos! ahí van: \n"+mascotas[0].nombre+" Una gata de color "+mascotas[0].color+" con "+mascotas[0].edad+" de edad.\n"+
-    mascotas[1].nombre+" Una gata de color "+mascotas[1].color+" con "+mascotas[1].edad+" de edad.\n"+
-    mascotas[2].nombre+" Una gata de color "+mascotas[2].color+" con "+mascotas[2].edad+" de edad.")
-} else {
-    alert("Input inválido. Intentá de nuevo")
-}*/
 
 //Filtrar por color de gato
 const filtrarPorColorN = mascotas.filter((mascota) => mascota.color == "Negro");
@@ -118,3 +100,20 @@ console.log(mascotas);
 //Más viejos a jovenes
 mascotas.sort((a,b) => b.edad - a.edad);
 console.log(mascotas);
+
+//Esta iteración es lo que se usaría con DOM para no tener un do while todo turbio.
+/*
+let buscarGato = prompt("Ingrese el nombre del gato (Ingrese FIN para cerrar)");
+if((buscarGato == "LENA") || (buscarGato == "Lena" || (buscarGato == "lena"))){
+    alert("Elegiste a "+mascotas[0].nombre+" Una gata de color "+mascotas[0].color+" con "+mascotas[0].edad+" de edad.")
+} else if ((buscarGato == "ROGER") || (buscarGato == "Roger" || (buscarGato == "roger"))){
+    alert("Elegiste a "+mascotas[1].nombre+" Un gato de color "+mascotas[1].color+" con "+mascotas[1].edad+" de edad.")
+} else if ((buscarGato == "KILLI") || (buscarGato == "Killi" || (buscarGato == "killi"))){
+    alert("Elegiste a "+mascotas[2].nombre+" Una gata de color "+mascotas[2].color+" con "+mascotas[2].edad+" de edad.")
+} else if ((buscarGato == "TODOS") || (buscarGato == "Todos" || (buscarGato == "todos"))){
+    alert("¡Elegiste a todos! ahí van: \n"+mascotas[0].nombre+" Una gata de color "+mascotas[0].color+" con "+mascotas[0].edad+" de edad.\n"+
+    mascotas[1].nombre+" Una gata de color "+mascotas[1].color+" con "+mascotas[1].edad+" de edad.\n"+
+    mascotas[2].nombre+" Una gata de color "+mascotas[2].color+" con "+mascotas[2].edad+" de edad.")
+} else {
+    alert("Input inválido. Intentá de nuevo")
+}*/
