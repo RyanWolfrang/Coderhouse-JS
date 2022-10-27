7422e1188558a9eec6407aea9d6991470e8339d8
Ryan <agustinrubertoni@gmail.com>
2022-10-27 19:35:36 -0300

    Agregué local storage, y los gatos no se amontonan cuando haces click a los botones.

--- js/script.js
+++ js/script.js
@@ -3,11 +3,12 @@ var contadorID = 1;
 
 class creadorMascotas{
     //Constructor con los arrays del gato
-    constructor(nombreid, nombre, edad, color, peso, imagen){
+    constructor(nombreid, nombre, edad, descripcion, color, peso, imagen){
         this.id = contadorID++
         this.nombreid = nombreid
         this.nombre = nombre;
         this.edad = edad;
+        this.descripcion = descripcion;
         this.color = color;
         this.peso = parseFloat(peso);
         this.imagen = imagen
@@ -16,13 +17,17 @@ class creadorMascotas{
 
 const mascotas = [];
 
-mascotas.push(new creadorMascotas("LENA","Lena", 2, "Negro", "3.200", "img/lena-gata-de-color-negro.jpeg"));
-mascotas.push(new creadorMascotas("ROGER","Roger", 8,"Negro", "7.800", "img/lena-gata-de-color-negro.jpeg"));
-mascotas.push(new creadorMascotas("KILLI","Killi", 2,"Tricolor", "3.800", "img/lena-gata-de-color-negro.jpeg"));
-mascotas.push(new creadorMascotas("GANGA","Ganga", 2,"Blanco y negro", "3.600", "img/lena-gata-de-color-negro.jpeg"));
+mascotas.push(new creadorMascotas("LENA","Lena", 2,"Descripción WIP", "Negro", "3.200", "img/lena-gata-de-color-negro.jpeg"));
+mascotas.push(new creadorMascotas("ROGER","Roger", 8,"Descripción WIP","Negro", "7.800", "img/lena-gata-de-color-negro.jpeg"));
+mascotas.push(new creadorMascotas("KILLI","Killi", 2,"Descripción WIP","Tricolor", "3.800", "img/lena-gata-de-color-negro.jpeg"));
+mascotas.push(new creadorMascotas("GANGA","Ganga", 2,"Descripción WIP","Blanco y negro", "3.600", "img/lena-gata-de-color-negro.jpeg"));
 
-//FILTROS
+//REALIZAR UN JS APARTE CON CADA DESCRIPCIÓN DEL GATO
 
+//Meter gatos en el local storage directamente stringificados (?)
+localStorage.setItem("mascotas", JSON.stringify(mascotas));
+
+//FILTROS WIP
 
 // //Filtrar por color de gato
 // const filtrarPorColorN = mascotas.filter((mascota) => mascota.color == "Negro");
@@ -42,23 +47,25 @@ mascotas.push(new creadorMascotas("GANGA","Ganga", 2,"Blanco y negro", "3.600", 
 // mascotas.sort((a,b) => b.edad - a.edad);
 // console.log(mascotas);
 
-// var mostrarGatos = document.getElementById("gato");
-
-
-//Funciones con botones para seleccionar cada gato
+// Parsear animales para poder usar en el código
+var animalesLocales = JSON.parse(localStorage.getItem("mascotas"));
 
 const mascotaSeleccionada = [];
 
 let botonesGatos = document.getElementById("botongatos");
 
 function detectarGato(){
-    mascotas.forEach((mascota)=>{
+    animalesLocales.forEach((mascota)=>{
+        //Crear boton | Darle id al botón | Darle clase al botón | Darle el nombre de cada gato al botón.
         let botonNuevo = document.createElement("button")
         botonNuevo.id = `btn${mascota.id}`
         botonNuevo.className = "btn btn-primary"
         botonNuevo.innerText = `${mascota.nombreid}`
 
         botonNuevo.addEventListener("click",function(){
+            //Dinamitar completamente el HTML donde se muestran los gatos para que no se amontonen.
+            mostrarGatoSingular.innerHTML = ""
+            //La función SIEMPRE va a tener que ir abajo por el efecto cascada.
             devolverGatoSeleccionado(mascota);
         })
         botonesGatos.append(botonNuevo)
@@ -70,9 +77,11 @@ detectarGato();
 let htmlDeGato = document.getElementById("mostrarGatoSingular")
 
 function devolverGatoSeleccionado(mascotaAMostrar){
-    //Poner un comando para eliminar todo de la array
+
+    //Agrega la mascota seleccionada a este array
     mascotaSeleccionada.push(mascotaAMostrar)
     
+    //Crear un div | Darle id | Darle clase | HTML para mostrar al gato
     let claseMostrarGato = document.createElement("div")
     claseMostrarGato.id = `gatoMostrado`
     claseMostrarGato.className = "columnas"
@@ -81,9 +90,7 @@ function devolverGatoSeleccionado(mascotaAMostrar){
     <div class="textoizq">
     <h3>${mascotaAMostrar.nombre}. EDAD: ${mascotaAMostrar.edad} años.</h3>
         <p>
-            ARRAY CON LA DESC DEL GATOO
-            OOOOOOOOOOOOOOOOOOOOOO
-            OOOOOOOOOOOOOOOOOOO
+            ${mascotaAMostrar.descripcion}
         </p>
     </div>
     <div class="imgdesktop">
