afca6d515951121c00c6c5221d8931f5eae45e2d
Ryan <agustinrubertoni@gmail.com>
2022-11-07 15:46:46 -0300

    Integración de una base de datos propia, con su fetch y optimización de código

--- js/script.js
+++ js/script.js
@@ -1,56 +1,32 @@
-
-var contadorID = 1;
-
-class creadorMascotas{
-    //Constructor con los arrays del gato
-    constructor(nombreid, nombre, edad, descripcion, color, peso, imagen){
-        this.id = contadorID++
-        this.nombreid = nombreid
-        this.nombre = nombre;
-        this.edad = edad;
-        this.descripcion = descripcion;
-        this.color = color;
-        this.peso = parseFloat(peso);
-        this.imagen = imagen
-    }
-}
-
-const mascotas = [];
-
-mascotas.push(new creadorMascotas("LENA","Lena", 2,descLena, "Negro", "3.200", "img/lena-gata-de-color-negro.jpeg"));
-mascotas.push(new creadorMascotas("ROGER","Roger", 8,descRoger,"Negro", "7.800", "img/lena-gata-de-color-negro.jpeg"));
-mascotas.push(new creadorMascotas("KILLI","Killi", 2,descKilli,"Tricolor", "3.800", "img/lena-gata-de-color-negro.jpeg"));
-mascotas.push(new creadorMascotas("GANGA","Ganga", 2,descGanga,"Blanco y negro", "3.600", "img/lena-gata-de-color-negro.jpeg"));
-
-//Meter gatos en el local storage directamente stringificados
-localStorage.setItem("mascotas", JSON.stringify(mascotas));
-
-// Parsear animales para poder usar en el código
-var animalesLocales = JSON.parse(localStorage.getItem("mascotas"));
-
 const mascotaSeleccionada = [];
 
-let botonesGatos = document.getElementById("botongatos");
-
-function detectarGato(){
-    animalesLocales.forEach((mascota)=>{
-        //Crear boton | Darle id al botón | Darle clase al botón | Darle el nombre de cada gato al botón.
-        let botonNuevo = document.createElement("button")
-        botonNuevo.id = `btn${mascota.id}`
-        botonNuevo.className = "btn btn-primary"
-        botonNuevo.innerText = `${mascota.nombreid}`
-
-        botonNuevo.addEventListener("click",function(){
-            //Dinamitar completamente el HTML donde se muestran los gatos para que no se amontonen.
-            mostrarGatoSingular.innerHTML = ""
-            //La función SIEMPRE va a tener que ir abajo por el efecto cascada.
-            devolverGatoSeleccionado(mascota);
-        })
-        botonesGatos.append(botonNuevo)
+function mostrarGatos(){
+    const URLJSON="js/mascotas.json";
+    fetch(URLJSON)
+        .then(res => res.json())
+        .then(datosMascotas => {
+            const mascotas = datosMascotas.mascotas;
+            mascotas.forEach((mascota)=>{
+                //Crear boton | Darle id al botón | Darle clase al botón | Darle el nombre de cada gato al botón.
+                let botonNuevo = document.createElement("button")
+                botonNuevo.id = `btn${mascota.id}`
+                botonNuevo.className = "btn btn-primary"
+                botonNuevo.innerText = `${mascota.nombre}`
+        
+                botonNuevo.addEventListener("click",function(){
+                    //Dinamitar completamente el HTML donde se muestran los gatos para que no se amontonen.
+                    mostrarGatoSingular.innerHTML = ""
+                    //Esta función SIEMPRE abajo
+                    devolverGatoSeleccionado(mascota);
+                })
+                botonesGatos.append(botonNuevo)
+            })
     })
 }
 
-detectarGato();
+mostrarGatos();
+
+let botonesGatos = document.getElementById("botongatos");
 
 let htmlDeGato = document.getElementById("mostrarGatoSingular")
 
@@ -82,6 +58,8 @@ function devolverGatoSeleccionado(mascotaAMostrar){
     `
     htmlDeGato.append(claseMostrarGato)
 }
+
+
 
 function actualizarReloj() {
     let reloj = document.getElementById('reloj');
